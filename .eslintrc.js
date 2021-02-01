e51c4e72c221b4c3f0b0fce46d22b37459072a0e
Amilkhael <amilkhael@outlook.com>
2021-02-01 17:06:39 -0600

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
   extends: ['airbnb'],
+  parser: "babel-eslint",
+  parserOptions: {
+    "sourceType": "module",
+    "allowImportExportEverywhere": true
+  },
   rules: {
     'react/jsx-filename-extension': 'off',
   },
+  env: {
+    "jest": true,
+    "browser": true
+  }
 };
