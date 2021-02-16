6582338966b2f0a2f8205d835e9adccc7e317499
Amilkhael <amil@enconta.com>
2021-02-15 23:37:45 -0600

    test fox

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   "./src/*.{js,jsx,ts,tsx}": [
     "npx prettier --write",
-    "eslint src/*.js --fix"
+    "eslint src/*.js --fix-dry-run",
+    "eslint --plugin eslint-plugin-import"
   ],
   "*.scss": [
     "stylelint --fix --syntax=scss",
