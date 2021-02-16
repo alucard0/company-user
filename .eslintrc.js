19f0ebf0adada2b28f4581e22b0be5609f0a4a48
Amilkhael <amil@enconta.com>
2021-02-15 19:47:43 -0600

    configurar eslint import

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,27 @@ module.exports = {
     "allowImportExportEverywhere": true
   },
   rules: {
+    "semi": "off",
     'react/jsx-filename-extension': 'off'
   },
   env: {
     "jest": true,
     "browser": true
+  },
+  settings: {
+    'import/order': ['warn', { 
+      'groups': ['builtin', 'external', 'internal', 'parent', 'sibling'], 
+      'alphabetize': {
+        order: 'asc'
+      },
+      "pathGroups": [
+        {
+          "pattern": "react",
+          "group": "builtin",
+          "position": 'before'
+        }
+      ],
+      "pathGroupsExcludedImportTypes": ["builtin"]
+    }]
   }
 }
